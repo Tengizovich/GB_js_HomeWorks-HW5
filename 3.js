85b39f1509936d49b5bb96d261488604d0d27170
Tengizovich <113883231+Tengizovich@users.noreply.github.com>
2023-09-07 21:46:05 +0300

    Create 3.js

---
+++ 3.js
@@ -0,0 +1,19 @@
+// Задание 3
+const products = [
+   {
+      id: 3,
+      price: 200,
+   },
+   {
+      id: 4,
+      price: 900,
+   },
+   {
+      id: 1,
+      price: 1000,
+   },
+];
+products.forEach(element => {
+   element.price -= element.price * (15 / 100);
+});
+console.log(products);
